a1d45cf169fbcddeabe4d80a86a2244dcf64fb7f
Tomáš Dvořák <lump2003@gmail.com>
2024-04-14 22:38:34 +0200

    prepared exponential funkction

---
+++ src/main/java/functions/ExponentialFunction.java
@@ -0,0 +1,18 @@
+package functions;
+
+import org.apache.commons.math3.analysis.UnivariateFunction;
+
+public class ExponentialFunction implements UnivariateFunction {
+    private final double a; // Amplituda funkce
+    private final double b; // Exponent
+
+    public ExponentialFunction(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+    public static final String regex = "y\\s*=\\s*(\\d*\\.?\\d+)\\s*\\*\\s*e\\^\\(([-+]?\\d*\\.?\\d+)x\\)";
+    @Override
+    public double value(double x) {
+        return a * Math.exp(b * x);
+    }
+}
